9e69aef0e8de692628def6b5fda66c1348f2d501
songcser <songjiyi2008@163.com>
2018-06-17 01:05:24 +0800

    add axisview and wordmask

--- src/views/meetings/Home.vue
+++ src/views/meetings/Home.vue
@@ -6,58 +6,7 @@
     <v-container fluid grid-list-lg>
       <v-layout row wrap>
         <v-flex xs8>
-          <div class="headline">大会背景</div>
-          <v-divider class="my-3"></v-divider>
-          <v-layout row wrap>
-            <v-flex xs12>
-              <v-card color="cyan darken-2" class="white--text">
-                <v-container fluid grid-list-lg>
-                  <v-layout row>
-                    <v-flex xs7>
-                      <div>
-                        <div>柴油机热效率高，动力性强，能减少高达45%的温室气体排放，但是其燃烧产生的氮氧化物(NOx)和颗粒物（PM)却是PM2.5最大来源。占汽车保有量仅7.8%的柴油货车，排放了57.3%的NOx和77.8%的PM，是机动车污染防治的重中之重。故2018年国家各部委相继把重型货车的污染治理当作污染防治攻坚战的重点。</div>
-                      </div>
-                    </v-flex>
-                    <v-flex xs5>
-                      <v-card-media :src="beiying1" height="125px" contain></v-card-media>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
-            <v-flex xs12>
-              <v-card color="teal darken-2" class="white--text">
-                <v-container fluid grid-list-lg>
-                  <v-layout row>
-                    <v-flex xs7>
-                      <div>
-                        <div>按照最新政策，国六a排放限值方案将于2020年执行，国六b标准将于2023年实施——在这样严峻的形势下，无论是商用车、非道路移动机械、发动机生产企业，还是排放技术的专业提供商，都需要加大节能减排技术的研发更新，甚至提前在可替代能源和新能源的产品上进行布局，为更严格的排放法规做好准备。</div>
-                      </div>
-                    </v-flex>
-                    <v-flex xs5>
-                      <v-card-media :src="beiying2" height="125px" contain></v-card-media>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
-            <v-flex xs12>
-              <v-card color="green darken-2" class="white--text">
-                <v-container fluid grid-list-lg>
-                  <v-layout row>
-                    <v-flex xs7>
-                      <div>
-                        <div>未来柴油机峰会系列2019，将立足于中国柴油发动机行业的实际情况，探讨业内人士比较关心的节能减排政策法规和最新的技术动向以及未来的发展；这里还将是一个您与政府高层、OEM制造商、行业领袖和专家进行国际项目交流的最佳平台。</div>
-                      </div>
-                    </v-flex>
-                    <v-flex xs5>
-                      <v-card-media :src="beiying3" height="125px" contain></v-card-media>
-                    </v-flex>
-                  </v-layout>
-                </v-container>
-              </v-card>
-            </v-flex>
-          </v-layout>
+          <word-mask />
           <div mt-5 class="headline">峰会亮点</div>
           <v-divider class="my-3"></v-divider>
           <v-layout row wrap>
@@ -107,11 +56,13 @@
 import VueSeamlessScroll from "vue-seamless-scroll";
 
 import AppAside from '@/views/meetings/AppAside'
+import WordMask from '@/views/meetings/WordMask'
 export default {
   name: "home",
   components: {
     VueSeamlessScroll,
-    AppAside
+    AppAside,
+    WordMask,
   },
   data() {
     return {
